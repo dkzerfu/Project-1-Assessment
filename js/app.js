e42564bf1a452e4d9708cbd058ba0d51d69b5389
dkzerfu <dkzerfu@gmail.com>
2021-02-12 17:34:14 -0500

    not done

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+let plusButton = document.querySelector('.add');
+let minusButton = document.querySelector('.minus');
+let input = document.querySelector('.input');
+let display = document.querySelector('.value');
+
+
+input.addEventListener('input', updateValue)
+var initialValue = 0;
+var currentValue;
+var maxInput = 200;
+
+console.log(currentValue)
+display.innerText = initialValue;
+
+plusButton.addEventListener('click', () => {
+    if (currentValue >= maxInput){
+        display.innerText = currentValue 
+    }else{
+        display.innerText = currentValue ++;
+
+    }
+
+
+})
+minusButton.addEventListener('click', () => {
+    if (currentValue >= maxInput){
+        display.innerText = currentValue--
+    }else{
+
+        display.innerText = currentValue --;
+    }
+
+})
+function updateValue(e){
+    currentValue = input.value
+    console.log(currentValue)
+    return currentValue;
+}
